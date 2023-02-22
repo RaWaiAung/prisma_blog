cb22507b3601b6313a77b6a4699f444de6ba885e
Ra Wai Aung <rawaiucsm@gmail.com>
2023-02-22 11:48:12 +0630

    image check

---
+++ src/utilities/imageCheck.ts
@@ -0,0 +1,15 @@
+// const fileForUpload = req.files.image as UploadedFile;
+// const rawFileName = fileForUpload.name.split(".");
+// const imageFileName = rawFileName[0];
+
+// function createImage(name) {
+//   if (name.match(/\.jpe?g$/)) {
+//     console.log("jpeg image");
+//   } else if (name.match(/\.gif$/)) {
+//     console.log("gif image");
+//   } else if (name.match(/\.png$/)) {
+//     console.log("png image");
+//   } else {
+//     console.log("Unsupported format");
+//   }
+// }
